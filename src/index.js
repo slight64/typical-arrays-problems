4f95ba9d04154aa105e775d81ba5a13bdbf35acd
slight64 <slight1989@gmail.com>
2021-03-05 02:25:31 +0400

    fix: fix functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return array === 0 ? 0 : array.reduce((a,e) => a < e ? a : e);
+  return !array ? 0 : array.reduce((a,e) => a < e ? a : e, 0);
 }
 
 exports.max = function max (array) {
-  return array === 0 ? 0 : array.reduce((a,e) => a > e ? a : e);
+  return !array ? 0 : array.reduce((a,e) => a > e ? a : e, 0);
 }
 
 exports.avg = function avg (array) {
-  return array.length === 0 ? 0 : (array.reduce((a,e) => a + e))/array.length;
+  return array === undefined || array == 0  || array == '' ? 0 : array.reduce((a,e) => a + e)/array.length
 }
