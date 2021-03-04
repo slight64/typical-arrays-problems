1f6db7d93fe10f32fa5c8456253d4657aa31c36d
slight64 <slight1989@gmail.com>
2021-03-05 01:57:26 +0400

    feat: release min max and average funcrion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array === 0 ? 0 : array.reduce((a,e) => a < e ? a : e);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array === 0 ? 0 : array.reduce((a,e) => a > e ? a : e);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array.length === 0 ? 0 : (array.reduce((a,e) => a + e))/array.length;
 }
