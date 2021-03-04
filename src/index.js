609f914a9a9621872bcf69e764c91556c094f55b
slight64 <slight1989@gmail.com>
2021-03-05 02:27:37 +0400

    fix: edit all functions, add check for array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
 exports.min = function min (array) {
-  return !array ? 0 : array.reduce((a,e) => a < e ? a : e, 0);
+  return array === undefined || array == 0  || array == '' ? 0 : array.reduce((a,e) => a < e ? a : e, 0);
 }
 
 exports.max = function max (array) {
-  return !array ? 0 : array.reduce((a,e) => a > e ? a : e, 0);
+  return array === undefined || array == 0  || array == '' ? 0 : array.reduce((a,e) => a > e ? a : e, 0);
 }
 
 exports.avg = function avg (array) {
